1e8f0e0e54e055fc5432cb6a4203cd9acd8e7042
James McSweeny <mcsweenyjm@gmail.com>
2019-12-05 08:32:55 -0500

    Day 4 2019

---
+++ AdventOfCode/Solutions/2019/Day4.fs
@@ -0,0 +1,40 @@
+﻿namespace Solutions2019
+
+module Day4 =
+    open Common
+
+    let parseInput fileName = 
+        let line = 
+            fileName
+            |> readLines
+            |> Seq.head
+        let parts = line.Split('-')
+        int parts.[0], int parts.[1]
+
+    let doesNotDecrease = Seq.pairwise >> Seq.forall (fun (a, b) -> a <= b)
+    let hasAPair = Seq.countBy id >> Seq.exists (fun (_, c) -> c >= 2)
+    let hasSinglePair = Seq.countBy id >> Seq.exists(fun (_, c) -> c = 2)
+
+    [<Solution(2019, 4, 1)>]
+    let part1 fileName =
+        let (rangeStart, rangeEnd) = parseInput fileName
+        let allPasswords = seq {rangeStart..rangeEnd}
+
+        allPasswords
+        |> Seq.map string
+        |> Seq.filter doesNotDecrease
+        |> Seq.filter hasAPair
+        |> Seq.length
+
+    [<Solution(2019, 4, 2)>]
+    let part2 fileName =
+        let (rangeStart, rangeEnd) = parseInput fileName
+        let allPasswords = seq {rangeStart..rangeEnd}
+
+        allPasswords
+        |> Seq.map string
+        |> Seq.filter doesNotDecrease
+        |> Seq.filter hasSinglePair
+        |> Seq.length
+
+    
