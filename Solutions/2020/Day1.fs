0ac1960efe8bfad5f8f82f842aa9f165bdb6f7b9
James McSweeny <mcsweenyjm@gmail.com>
2020-12-03 18:15:34 -0500

    Day 1 and Day 2

---
+++ Solutions/2020/Day1.fs
@@ -0,0 +1,29 @@
+﻿namespace Soltions2020
+
+module Day1 = 
+
+    open Common
+
+    let rec combinations n l = 
+        match n, l with
+        | 0, _ -> [[]]
+        | _, [] -> []
+        | k, (x::xs) -> List.map ((@) [x]) (combinations (k-1) xs) @ combinations k xs
+        
+    [<Solution(2020, 1, 1)>]
+    let part1 fileName =
+        fileName
+        |> readLinesAs int
+        |> Seq.toList
+        |> combinations 2
+        |> Seq.find (fun p -> p |> Seq.sum = 2020)
+        |> Seq.fold (*) 1
+
+    [<Solution(2020, 1, 2)>]
+    let part2 fileName =
+        fileName
+        |> readLinesAs int
+        |> Seq.toList
+        |> combinations 3
+        |> Seq.find (fun p -> p |> Seq.sum = 2020)
+        |> Seq.fold (*) 1
---
+++ Solutions/2020/Day2.fs
@@ -0,0 +1,52 @@
+﻿namespace Soltions2020
+
+module Day2 = 
+
+    open Common
+
+    type Policy = {
+        minOccurrences: int;
+        maxOccurrences: int;
+        requiredLetter: char;
+        password: string;
+    }
+
+    let parseLineAsPolicy (line: string) =
+        let parts = line.Split(' ')
+        let occurrences = parts.[0].Split('-')
+        let requiredLetter = parts.[1].[0]
+        let password = parts.[2]
+        {
+            minOccurrences = int occurrences.[0];
+            maxOccurrences = int occurrences.[1];
+            requiredLetter = requiredLetter;
+            password = password;
+        }
+
+    let countOccurrences x = Seq.filter ((=) x) >> Seq.length
+
+    let isValidPassword (policy: Policy) =
+        let occurrences = policy.password |> countOccurrences policy.requiredLetter
+        occurrences >= policy.minOccurrences && occurrences <= policy.maxOccurrences
+
+    let isValidPassword2 (policy: Policy) =
+        [policy.password.[policy.minOccurrences - 1]; policy.password.[policy.maxOccurrences - 1]]
+        |> countOccurrences policy.requiredLetter
+        |> (=) 1
+
+        
+    [<Solution(2020, 2, 1)>]
+    let part1 fileName =
+        fileName
+        |> readLinesAs parseLineAsPolicy
+        |> Seq.filter isValidPassword
+        |> Seq.length
+
+    [<Solution(2020, 2, 2)>]
+    let part2 fileName =
+        fileName
+        |> readLinesAs parseLineAsPolicy
+        |> Seq.filter isValidPassword2
+        |> Seq.length
+
+
