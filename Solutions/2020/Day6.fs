cfb678850d40cffaa12b3fc7d1916e9c85249eef
James McSweeny <mcsweenyjm@gmail.com>
2020-12-06 10:31:48 -0500

    Day 6

---
+++ Solutions/2020/Day6.fs
@@ -0,0 +1,39 @@
+﻿namespace Soltions2020
+
+module Day6 = 
+    open Common
+    open System
+
+    let parseGroups = 
+        split (Environment.NewLine + Environment.NewLine)
+        >> Seq.map (replace Environment.NewLine " ")
+        >> Seq.map (split " ")
+
+    let findAggregate = Seq.fold (+) "" >> Seq.distinct >> Seq.length
+
+    let findCommon numPeople = 
+        Seq.fold (+) "" 
+        >> Seq.groupBy id 
+        >> Seq.filter (fun (_, l) -> numPeople = (l |> Seq.length)) 
+        >> Seq.map fst 
+        >> Seq.length
+
+    let findCommonInGroup group = 
+        let numPeople = group |> Seq.length
+        group |> findCommon numPeople
+        
+    [<Solution(2020, 6, 1)>]
+    let part1 fileName =
+        fileName
+        |> readText
+        |> parseGroups
+        |> Seq.map findAggregate
+        |> Seq.sum
+
+    [<Solution(2020, 6, 2)>]
+    let part2 fileName = 
+        fileName
+        |> readText
+        |> parseGroups
+        |> Seq.map findCommonInGroup
+        |> Seq.sum
