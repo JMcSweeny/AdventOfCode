4a48a4c0936fd122f21ebfadcf103f65e3d6abf3
James McSweeny <mcsweenyjm@gmail.com>
2022-12-04 14:18:14 -0500

    2022 Day 1

---
+++ Solutions/2022/Day1.fs
@@ -0,0 +1,31 @@
+﻿namespace Soltions20122
+
+module Day1 =
+    open Common
+
+    let calculateCalorie (curr, totals) calorie =
+        match calorie with
+            | "" -> (0, curr :: totals)
+            | _ -> (curr + (int calorie), totals)
+
+    let topThreeTotal = Seq.sortDescending >> Seq.take 3 >> Seq.sum
+
+    let parseInput fileName = 
+        fileName
+        |> readLines
+
+    [<Solution(2022, 1, 1)>]
+    let part1 fileName =
+        fileName
+        |> parseInput
+        |> Seq.fold calculateCalorie (0, List.Empty)
+        |> snd
+        |> Seq.max
+
+    [<Solution(2022, 1, 2)>]
+    let part2 fileName =
+        fileName
+        |> parseInput
+        |> Seq.fold calculateCalorie (0, List.Empty)
+        |> snd
+        |> topThreeTotal
