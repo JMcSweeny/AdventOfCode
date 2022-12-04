6920f4538900f0fef91595704f6819a32682ebb1
James McSweeny <mcsweenyjm@gmail.com>
2022-12-04 17:09:26 -0500

    Days 2 and 3

---
+++ Solutions/2022/Day2.fs
@@ -0,0 +1,42 @@
+﻿namespace Soltions20122
+
+module Day2 =
+    open Common
+
+    let calculateScore line =
+        match line with
+        | "A X" -> 4
+        | "A Y" -> 8
+        | "A Z" -> 3
+        | "B X" -> 1
+        | "B Y" -> 5
+        | "B Z" -> 9
+        | "C X" -> 7
+        | "C Y" -> 2
+        | "C Z" -> 6
+        | _ -> 0
+
+    let calculateScoreTwo line =
+        match line with
+        | "A X" -> 3
+        | "A Y" -> 4
+        | "A Z" -> 8
+        | "B X" -> 1
+        | "B Y" -> 5
+        | "B Z" -> 9
+        | "C X" -> 2
+        | "C Y" -> 6
+        | "C Z" -> 7
+        | _ -> 0
+
+    [<Solution(2022, 2, 1)>]
+    let part1 fileName =
+        fileName
+        |> readLinesAs calculateScore
+        |> Seq.sum
+
+    [<Solution(2022, 2, 2)>]
+    let part2 fileName =
+        fileName
+        |> readLinesAs calculateScoreTwo
+        |> Seq.sum
---
+++ Solutions/2022/Day3.fs
@@ -0,0 +1,23 @@
+﻿namespace Soltions20122
+
+module Day3 =
+    open Common
+
+    let priorityValues = ['a'..'z'] @ ['A'..'Z']
+
+    let toPriority item =
+        Seq.findIndex ((=) item) priorityValues + 1
+
+    [<Solution(2022, 3, 1)>]
+    let part1 fileName =
+        fileName
+        |> readLinesAs (Seq.splitInto 2)
+        |> Seq.sumBy (Seq.map Set >> Set.intersectMany >> Seq.sumBy toPriority)
+
+    [<Solution(2022, 3, 2)>]
+    let part2 fileName =
+        fileName
+        |> readLines
+        |> Seq.chunkBySize 3
+        |> Seq.collect (Seq.map Set >> Set.intersectMany)
+        |> Seq.sumBy toPriority
