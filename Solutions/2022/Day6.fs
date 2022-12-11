6e9f43f7793fbeefd33da7e6a5abc998fd64156a
James McSweeny <mcsweenyjm@gmail.com>
2022-12-11 14:33:20 -0500

    Day 6

---
+++ Solutions/2022/Day6.fs
@@ -0,0 +1,27 @@
+﻿namespace Soltions20122
+
+module Day6 =
+    open Common
+
+    let hasDuplicates (window: int) (chars: seq<char>) =
+        chars |> Seq.distinct |> Seq.length < window
+
+    let getMarker (window: int) (buffer: string) =
+        buffer
+        |> Seq.windowed window
+        |> Seq.takeWhile (hasDuplicates window)
+        |> Seq.length
+        |> (fun l -> l + window)
+
+
+    [<Solution(2022, 6, 1)>]
+    let part1 fileName =
+        fileName
+        |> readText
+        |> getMarker 4
+
+    [<Solution(2022, 6, 2)>]
+    let part2 fileName =
+        fileName
+        |> readText
+        |> getMarker 14
